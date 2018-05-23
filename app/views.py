c9e623d42684ab374b40d7677d0c6b2a089b9da6
lxr <liangxuanran@126.com>
2018-05-23 15:10:54 +0000

    my first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,24 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, flash
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', title = 'mama', user={'nickname' : 'xuanxuan'})
+	title = 'index'
+	user = {'nickname' : 'xuanxuan'}
+	posts = [
+		{'name': 'lxr','body':'1231123'},
+		{'name' : 'zj', 'body':'456456'}	
+	]
+	return render_template('index.html', title = title, user = user, posts = posts)
+
+@app.route('/login', methods = {'GET' , 'POST'})
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash("Login request for openid= " + form.openid.data + ", remember_me = " + str(form.remember_me.data))
+		return redirect("/index")
+		
+	return render_template('login.html', title = "Sign In", form = form, providers = app.config['OPENID_PROVIDERS']) 	
---
+++ config.py
@@ -0,0 +1,4 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'you_will_never_guess'
+
+OPENID_PROVIDERS = [{'name':'Flickr', 'url':'https://www.flickr.com/<username>'},{'name':'Yahoo','url':'https://me.yahoo.com'}]
